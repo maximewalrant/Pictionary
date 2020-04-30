5d2de87fceb13d17c2aebe29caa968728cafb3cf
maximewalrant <47498015+maximewalrant@users.noreply.github.com>
2020-04-30 13:57:34 +0200

    HTML

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 'use strict';
 
-const express = required('express');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 3000;
+const index = '/pictionary.html';
+
+const server = express()
+  .use((req, res) =>
+    res.sendFile(index, {
+      root: __dirname
+    }))
+
+  .listen(port, () => console.log('Server started on port', port));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('A new user joined the game');
+
+  onConnection(socket);
+});
+
+function onConnection(socket) {
+  socket.on('username', (username) => {
+    console.log('Player name:', username);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user left the game');
+  });
+
+  socket.on('line', data => {
+    socket.broadcast.emit('line', data);
+  });
+}
